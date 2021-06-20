8e486246cf2182c4498862f20016da5c269f5708
Ashif <sshsmhsf@gmail.com>
2021-06-20 14:14:08 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./components/Layout/Layout";
+import { CssBaseline } from "@material-ui/core";
+import BestServices from "./components/BestServices/BestServices";
+import PropertiesType from "./components/PropertiesType/PropertiesType";
+import OurPartners from "./components/OurPartners/OurPartners";
+import Connect from "./components/Connect/Connect";
+import Booking from "./components/Booking/Booking";
+import WelcomeBanner from "./components/WelcomeBanner/WelcomeBanner";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Layout>
+        <WelcomeBanner />
+        <PropertiesType />
+        <BestServices />
+        <Booking />
+        <OurPartners />
+        <Connect />
+      </Layout>
+    </>
   );
 }
 
